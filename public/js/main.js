2af2391d055e314e1a232563a5216147d00c498b
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-21 11:00:20 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+/*
+
+for (let i = 1; i <=10;i++) {
+    console.log(i);
+}
+
+*/
+
+/*
+let num = 10;
+
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+*/
+/*
+nb=0;
+do {
+    console.log(nb);
+    nb+=2;
+} while (nb<=20);
+*/
+/*
+let tab=['Bonjour','aurevoir','merci']
+
+for (const element of tab) {
+    console.log(element);
+}
+*/
+
+/*
+let somme=0;
+for (let nbr = 0; nbr < 10; nbr++) {
+    somme += nbr;
+}
+console.log(somme);
+*/
+
+/*
+
+let user;
+user=  prompt(`Entrez un nombre`)
+while (user<=0) {
+    user=  prompt(`Entrez un nombre`)
+}
+*/
+/*
+
+for (let nbr1 = 1; nbr1 <=10; nbr1++) {
+    console.log(nbr1);
+    if (nbr1==5) {
+        break;
+    }
+}
+*/
+
+
+for (let index = 1; index <=10; index++) {
+    
+    if (index%2==0) {
+        console.log(index);
+        continue;
+        
+    }
+    
+}
